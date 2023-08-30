144a2fd08d8e5342500bc0b6996698be920a36d9
sarker24 <sarker24@gmail.com>
2023-08-30 12:16:49 +0200

    add modal with form

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,18 +1,59 @@
+import { useForm } from "react-hook-form";
 import Modal from "../Model";
 
 
 const AddTaskModal = ({isOpen, setIsOpen}) => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+   
+    const onSubmit = data => {
+        console.log(data);
+    }
+   /* 
+   add react hook form and tailwindcss-forms
+    */
     return (
         <Modal  isOpen={isOpen} setIsOpen={setIsOpen} title="This is title">
-                    
-        <p>
-            similique odit? Laudantium veritatis adipisci ea id? Velit ad
-            tenetur nesciunt officia commodi vitae perferendis molestias ducimus 
-            quo aliquam rem delectus beatae blanditiis omnis explicabo labore
-            minima accusantium deserunt quia, esse architecto, culpa impedit.
-            Praesentium reiciendis quasi temporibus quos sit deserunt 
-        </p>
-    </Modal>
+       <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-1">
+            <label htmlFor='tilte'>Tilte</label>
+        <input className="w-full rounded-md" type="text" id='title' {...register('name')} />
+        </div>
+        <div className="flex flex-col gap-1">
+            <label htmlFor='tilte'>Description</label>
+        <textarea className="w-full rounded-md" type="text" id='description' {...register('description')} />
+        </div>
+        <div className="flex flex-col gap-1">
+            <label htmlFor='tilte'>Deadline</label>
+        <input className="w-full rounded-md" type="date" id='date' {...register('date')} />
+        </div>
+        <div className="flex flex-col gap-1">
+            <label htmlFor='tilte'>AssignTo</label>
+            <select className="w-full rounded-md" id='assignTo' {...register("assignTo")}>
+                <option value="Mamun">Mamun</option>
+                <option value="Shafiquel">Shafiquel</option>
+                <option value="Kamrul">Kamrul</option>
+                <option value="Faruk">Faruk</option>
+
+            </select>
+        </div>
+        <div className="flex flex-col gap-1">
+            <label htmlFor='tilte'>Priority</label>
+            <select className="w-full rounded-md" id='priority' {...register("priority")}>
+                <option defaultValue value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+            </select>
+        </div>
+        <div  className="flex flex-row gap-1 justify-end mt-3 mr-1">
+        <button type="button" className="btn btn-danger mr-1">cancel</button>    
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </div>
+            
+       
+       </form>             
+       
+        </Modal>
     );
 };
 
